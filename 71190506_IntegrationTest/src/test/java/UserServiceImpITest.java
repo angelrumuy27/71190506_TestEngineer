61ebc82e6f07137449e0416dc927488fff9269d4
angelrumuy27 <89971608+angelrumuy27@users.noreply.github.com>
2022-05-14 22:34:51 +0700

    update

--- 71190506_IntegrationTest/src/test/java/UserServiceImpITest.java
+++ 71190506_IntegrationTest/src/test/java/UserServiceImpITest.java
@@ -12,18 +12,24 @@ public class UserServiceImpITest {
     SecurityService securityService = Mockito.mock(SecurityService.class);
 
     @Test
-    public void testAssignPassword() throws Exception {
+    public void testGetNewPass() throws Exception {
         //prepared SUT
         UserServiceImpl sut = new UserServiceImpl(userDAO, securityService);
-        user.getPassword();
-        Mockito.when(user.getPassword()).thenReturn("1234");
-        Mockito.when(userDAO.updateUser(user)).thenReturn("71190506");
-
-        System.out.println(user.getPassword());
-//        System.out.println(userDAO.updateUser(user));
-
+//        user.getPassword();
+//        Mockito.when(user.getPassword()).thenReturn("1234");
+//        System.out.println(user.getPassword());
         sut.assignPassword(user);
+        Mockito.verify(user).getPassword();
 
+
+    }
+
+    @Test
+    public void testUpdateUser() throws Exception {
+        UserServiceImpl sut = new UserServiceImpl(userDAO, securityService);
+//        Mockito.when(userDAO.updateUser(user)).thenReturn("71190506");
+//        System.out.println(userDAO.updateUser(user));
+        userDAO.updateUser(user);
         Mockito.verify(userDAO).updateUser(user);
 
 
